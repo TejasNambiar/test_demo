ee4e5be05695fc465ed2da100f148238acda35dd
Tejas <tejasnambiar32@gmail.com>
2021-07-28 16:14:43 +0530

    added GitCheck java

--- GitCheck.java
+++ GitCheck.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 class GitCheck {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hungry?");
+        String answer = sc.nextLine();
+        if(answer.equalsIgnoreCase("yes"))
+            System.out.println("come have some bread");
+        else
+            System.out.println("run along");
     }
 }
